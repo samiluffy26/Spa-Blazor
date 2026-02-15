5f6124bc86bc2e51dba57c440f25e580c41f9b8d
Samuel Guance <samuelisaiassanti944@gmail.com>
2026-02-15 14:19:48 -0400

    Added new funcionalities for de interactivity 3

--- spa-reservas-blazor.Application/Interfaces/IServiceRepository.cs
+++ spa-reservas-blazor.Application/Interfaces/IServiceRepository.cs
@@ -7,4 +7,6 @@ public interface IServiceRepository
     Task<List<Service>> GetAllAsync();
     Task<Service?> GetByIdAsync(string id);
     Task CreateAsync(Service service);
+    Task UpdateAsync(Service service);
+    Task DeleteAsync(string id);
 }
